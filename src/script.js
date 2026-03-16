4abaf20000b53c668a283469f182a65e8ec1399d
abel pujol <abelpujol99@gmail.com>
2026-03-16 13:02:30 +0100

    Add new architecture level

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 let categoryExpanded = null;
 let subcategoryExpanded = null;
+let cardExpanded = null;
 
 let closingCategory = false;
 
@@ -13,23 +14,11 @@ function DisplayNone(element)
     element.style.display = "none";
 }
 
-function AllWidth(element)
-{
-    element.style.width = "100%";
-}
-
 function OpenCategory()
 {
+    categoryExpanded.style.marginBottom = "0";
     DisplayFlex(categoryExpanded.querySelector('.category-dropdown-content'));
     RemoveCursorPointer(categoryExpanded);
-}
-
-function OpenSubcategory()
-{
-    subcategoryExpanded.style.width = "100%";
-    DisplayNone(subcategoryExpanded.querySelector('.subcategory-dropdown-banner'));
-    DisplayFlex(subcategoryExpanded.querySelector('.subcategory-dropdown-content'));
-    RemoveCursorPointer(subcategoryExpanded);
 }
 
 function CloseCategory()
@@ -40,10 +29,25 @@ function CloseCategory()
     }
     
     AddCursorPointer(categoryExpanded);
+    categoryExpanded.style.marginBottom = "1rem";
     DisplayNone(categoryExpanded.querySelector('.category-dropdown-content'));
     categoryExpanded = null;
     CloseSubcategory();
     setTimeout(() => closingCategory = false, 10);
+}
+
+function OpenSubcategory()
+{
+    subcategoryExpanded.style.width = "100%";
+    DisplayNone(subcategoryExpanded.querySelector('.card-dropdown-banner'));
+
+    let subcategoryFrame = subcategoryExpanded.querySelector('.subcategory-frame');
+    DisplayFlex(subcategoryFrame);
+    let subcategoryDropdwonContent = subcategoryFrame.querySelector('.category-dropdown-content');
+    DisplayFlex(subcategoryDropdwonContent);
+    subcategoryDropdwonContent.style.margin = "0";
+
+    RemoveCursorPointer(subcategoryExpanded);
 }
 
 function CloseSubcategory()
@@ -55,9 +59,38 @@ function CloseSubcategory()
 
     AddCursorPointer(subcategoryExpanded);
     subcategoryExpanded.style.width = "";
-    DisplayFlex(subcategoryExpanded.querySelector('.subcategory-dropdown-banner'));
-    DisplayNone(subcategoryExpanded.querySelector('.subcategory-dropdown-content'))
+    DisplayFlex(subcategoryExpanded.querySelector('.card-dropdown-banner'));
+
+    let subcategoryFrame = subcategoryExpanded.querySelector('.subcategory-frame');
+    DisplayNone(subcategoryFrame);
+    let subcategoryDropdwonContent = subcategoryFrame.querySelector('.category-dropdown-content');
+    DisplayNone(subcategoryDropdwonContent);
+    subcategoryDropdwonContent.style.margin = "1rem 0";
     subcategoryExpanded = null;
+    CloseCard();
+    setTimeout(() => closingCategory = false, 10);
+}
+
+function OpenCard()
+{
+    cardExpanded.style.width = "100%";
+    DisplayNone(cardExpanded.querySelector('.card-dropdown-banner'));
+    DisplayFlex(cardExpanded.querySelector('.card-dropdown-content'));
+    RemoveCursorPointer(cardExpanded);
+}
+
+function CloseCard()
+{
+    if(cardExpanded == null)
+    {
+        return;
+    }
+
+    AddCursorPointer(cardExpanded);
+    cardExpanded.style.width = "";
+    DisplayFlex(cardExpanded.querySelector('.card-dropdown-banner'));
+    DisplayNone(cardExpanded.querySelector('.card-dropdown-content'))
+    cardExpanded = null;
     setTimeout(() => closingCategory = false, 10);
 }
 
@@ -90,14 +123,14 @@ function AttachClickHandler()
         OpenCategory();
     }));
 
-    document.querySelectorAll('.close-category').forEach(close => close.addEventListener('click', (event) => {
+    document.querySelectorAll('#category-cross').forEach(close => close.addEventListener('click', (event) => {
 
         closingCategory = true;
         CloseCategory();
     }));
 
     document.querySelectorAll('.subcategory-dropdown').forEach(subcategoryDropdown => subcategoryDropdown.addEventListener('click', (event) => {
-
+        
         if(closingCategory || subcategoryDropdown == subcategoryExpanded)
         {
             return;
@@ -106,14 +139,31 @@ function AttachClickHandler()
         CloseSubcategory();
         subcategoryExpanded = subcategoryDropdown;
         OpenSubcategory();
-
     }));
 
-
-    document.querySelectorAll('.close-subcategory').forEach(close => close.addEventListener('click', (event) => {
+    document.querySelectorAll('#subcategory-cross').forEach(close => close.addEventListener('click', (event) => {
 
         closingCategory = true;
         CloseSubcategory();
+    }));
+
+    document.querySelectorAll('.card-dropdown').forEach(cardDropdown => cardDropdown.addEventListener('click', (event) => {
+
+        if(closingCategory || cardDropdown == cardExpanded)
+        {
+            return;
+        }
+
+        CloseCard();
+        cardExpanded = cardDropdown;
+        OpenCard();
+    }));
+
+
+    document.querySelectorAll('#card-cross').forEach(close => close.addEventListener('click', (event) => {
+
+        closingCategory = true;
+        CloseCard();
     }));
 }
 
